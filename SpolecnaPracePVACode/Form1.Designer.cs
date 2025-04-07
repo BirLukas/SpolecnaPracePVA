ef74bbd47689a40b87ec08beca5c4827269c76cb
Andrej Zdvořák <andrej.zdvorak.123@gmail.com>
2025-04-07 09:26:20 +0200

    ui added

--- SpolecnaPracePVACode/Form1.Designer.cs
+++ SpolecnaPracePVACode/Form1.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            CocktailImage = new PictureBox();
+            NewCocktailBtn = new Button();
+            Heading = new Label();
+            NameLabel = new Label();
+            GlassLabel = new Label();
+            CategoryLabel = new Label();
+            AlcoholicLabel = new Label();
+            InstructionsLabel = new Label();
+            IngredientsLabel = new Label();
+            ((System.ComponentModel.ISupportInitialize)CocktailImage).BeginInit();
+            SuspendLayout();
+            // 
+            // CocktailImage
+            // 
+            CocktailImage.BackgroundImageLayout = ImageLayout.Center;
+            CocktailImage.Location = new Point(11, 9);
+            CocktailImage.Name = "CocktailImage";
+            CocktailImage.Size = new Size(280, 280);
+            CocktailImage.TabIndex = 0;
+            CocktailImage.TabStop = false;
+            // 
+            // NewCocktailBtn
+            // 
+            NewCocktailBtn.BackColor = Color.Yellow;
+            NewCocktailBtn.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
+            NewCocktailBtn.Location = new Point(11, 295);
+            NewCocktailBtn.Name = "NewCocktailBtn";
+            NewCocktailBtn.Size = new Size(280, 37);
+            NewCocktailBtn.TabIndex = 1;
+            NewCocktailBtn.Text = "New random cocktail!";
+            NewCocktailBtn.UseVisualStyleBackColor = false;
+            // 
+            // Heading
+            // 
+            Heading.AutoSize = true;
+            Heading.Font = new Font("Segoe UI", 14F);
+            Heading.Location = new Point(297, 9);
+            Heading.Name = "Heading";
+            Heading.Size = new Size(342, 32);
+            Heading.TabIndex = 2;
+            Heading.Text = "Information about the cocktail:";
+            // 
+            // NameLabel
+            // 
+            NameLabel.AutoSize = true;
+            NameLabel.Font = new Font("Segoe UI", 12F);
+            NameLabel.Location = new Point(297, 48);
+            NameLabel.Name = "NameLabel";
+            NameLabel.Size = new Size(73, 28);
+            NameLabel.TabIndex = 3;
+            NameLabel.Text = "Name: ";
+            // 
+            // GlassLabel
+            // 
+            GlassLabel.AutoSize = true;
+            GlassLabel.Font = new Font("Segoe UI", 12F);
+            GlassLabel.Location = new Point(297, 76);
+            GlassLabel.Name = "GlassLabel";
+            GlassLabel.Size = new Size(66, 28);
+            GlassLabel.TabIndex = 4;
+            GlassLabel.Text = "Glass: ";
+            // 
+            // CategoryLabel
+            // 
+            CategoryLabel.AutoSize = true;
+            CategoryLabel.Font = new Font("Segoe UI", 12F);
+            CategoryLabel.Location = new Point(297, 104);
+            CategoryLabel.Name = "CategoryLabel";
+            CategoryLabel.Size = new Size(101, 28);
+            CategoryLabel.TabIndex = 5;
+            CategoryLabel.Text = "Category: ";
+            // 
+            // AlcoholicLabel
+            // 
+            AlcoholicLabel.AutoSize = true;
+            AlcoholicLabel.Font = new Font("Segoe UI", 12F);
+            AlcoholicLabel.Location = new Point(297, 132);
+            AlcoholicLabel.Name = "AlcoholicLabel";
+            AlcoholicLabel.Size = new Size(102, 28);
+            AlcoholicLabel.TabIndex = 6;
+            AlcoholicLabel.Text = "Alcoholic: ";
+            // 
+            // InstructionsLabel
+            // 
+            InstructionsLabel.AutoSize = true;
+            InstructionsLabel.Font = new Font("Segoe UI", 12F);
+            InstructionsLabel.Location = new Point(297, 213);
+            InstructionsLabel.Name = "InstructionsLabel";
+            InstructionsLabel.Size = new Size(122, 28);
+            InstructionsLabel.TabIndex = 7;
+            InstructionsLabel.Text = "Instructions: ";
+            // 
+            // IngredientsLabel
+            // 
+            IngredientsLabel.AutoSize = true;
+            IngredientsLabel.Font = new Font("Segoe UI", 12F);
+            IngredientsLabel.Location = new Point(297, 160);
+            IngredientsLabel.Name = "IngredientsLabel";
+            IngredientsLabel.Size = new Size(114, 28);
+            IngredientsLabel.TabIndex = 8;
+            IngredientsLabel.Text = "Ingredients:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(678, 344);
+            Controls.Add(IngredientsLabel);
+            Controls.Add(InstructionsLabel);
+            Controls.Add(AlcoholicLabel);
+            Controls.Add(CategoryLabel);
+            Controls.Add(GlassLabel);
+            Controls.Add(NameLabel);
+            Controls.Add(Heading);
+            Controls.Add(NewCocktailBtn);
+            Controls.Add(CocktailImage);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)CocktailImage).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox CocktailImage;
+        private Button NewCocktailBtn;
+        private Label Heading;
+        private Label NameLabel;
+        private Label GlassLabel;
+        private Label CategoryLabel;
+        private Label AlcoholicLabel;
+        private Label InstructionsLabel;
+        private Label IngredientsLabel;
     }
 }
